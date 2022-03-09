70c1355084a81accfb96d62afe8286ce2dcb8d06
Jill Kidman <jillkidman@Jills-MacBook-Air.local>
2022-03-09 16:20:50 -0500

    commit for assignment

---
+++ main.js
@@ -0,0 +1,23 @@
+let paragraph = document.querySelector("p");
+paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+paragraph.style.color = "lightblue";
+
+let heading = document.querySelector("h1");
+heading.style.fontSize = "48px";
+
+let itemThirteen = document.querySelector("#item-13");
+itemThirteen.style.opacity = "0.5";
+
+let itemThree = document.querySelector("#item-3");
+itemThree.innerText = 'I say, "Hi!"';
+
+let image = document.querySelector("img");
+image.src = ("http://www.tioxic.com/wp-content/uploads/trex_4.jpg");
+image.style.height = "300px";
+
+let imageTwo = document.querySelector("#baby");
+imageTwo.style.height = "300px";
+
+let itemSixteen = document.querySelector("#item-16");
+itemSixteen.innerText = "Won't get fooled again.";
